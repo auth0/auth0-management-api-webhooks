3d877680d2d3aae2ad795b74f05aad8d41919956
zxan1285 <nonexist.void@gmail.com>
2017-08-02 14:06:56 +0300

    hook fix, server-side filtering

--- server/lib/processLogs.js
+++ server/lib/processLogs.js
@@ -90,7 +90,8 @@ module.exports = (storage) =>
       clientSecret: config('AUTH0_CLIENT_SECRET'),
       batchSize: parseInt(config('BATCH_SIZE')),
       startFrom: config('START_FROM'),
-      logTypes: [ 'sapi', 'fapi' ]
+      logTypes: [ 'sapi', 'fapi' ],
+      serversideFiltering: config('AUTH0_RTA').replace('https://', '') === 'auth0.auth0.com'
     };
 
     if (!options.batchSize || options.batchSize > 100) {
